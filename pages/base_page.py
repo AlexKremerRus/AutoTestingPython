f47e18b563c78262572339cefe650465b79226cf
a.nekhoroshev <Alexey.Nekhoroshev@advalange.com>
2022-11-30 18:50:39 +0300

    update file base_page viewed 3 video

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,5 @@
-#from selenium.webdriver.support.ui import WebdriverWait as wait
-#from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
     def __init__(self,driver,url):
@@ -9,23 +9,27 @@ class BasePage:
     def open(self):
         self.driver.get(self.url)
 
+    def element_is_visible(self, locator,timeout=5):
+        return wait(self.driver,timeout).until(EC.visibility_of_element_located(locator))
 
-#
-# class BasePage:
-#     def __init__(self, driver, url):
-#         self.driver = driver
-#         self.url = url
-#
-#     def open(self):
-#         self.driver.get(self.url)
+    def elements_are_vissible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
-#    def element_is_vissible(self, locator, timeout=5):
-#        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+    def element_is_present(self, locator,timeout=5):
+        return wait(self.driver,timeout).until(EC.presence_of_element_located(locator))
 
-#    def elements_are_vissible(self, locator, timeout=5):
-#        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+    def element_are_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
 
-#    def element_is_presents(self, locator, timeout=5):
-#        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+    def element_is_not_visible(self, locator,timeout=5):
+        return wait(self.driver,timeout).until(EC.invisibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    def do_to_element(self, element):
+        self.driver.execute_script("argement[0].scrollIntoView();", element)
+
+
 # Даша удали этот коммент
 
