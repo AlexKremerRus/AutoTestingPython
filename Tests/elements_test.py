ab0b24dc5171d12f424334f534359bf18767586f
a.nekhoroshev <Alexey.Nekhoroshev@advalange.com>
2022-12-01 18:52:39 +0300

    add files

--- Tests/elements_test.py
+++ Tests/elements_test.py
@@ -9,7 +9,11 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open()
-            text_box_page.fill_all_fields()
+            full_name, email, current_address, permanent_address= text_box_page.fill_all_fields()
             output_name,output_email, output_cur_addr, output_per_addr=text_box_page.check_filled_form()
-            print(output_name,output_email, output_cur_addr, output_per_addr)
-            #time.sleep(5)
+
+
+            assert full_name== output_name, "the full name does not match"
+            assert email== output_email, "the email does not match"
+            assert current_address== output_cur_addr, "the current address does not match"
+            assert permanent_address== output_per_addr, "the permanent address does not match"
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,5 +1,6 @@
 import time
 
+from Generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -8,11 +9,17 @@ class TextBoxPage(BasePage):
     locators=TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys("sfgg")
-        self.element_is_visible(self.locators.EMAIL).send_keys("asd@asd.ds")
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys("asd")
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys("asd")
+        person_info=next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
+        return full_name, email, current_address,permanent_address
 
     def check_filled_form(self):
         full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
