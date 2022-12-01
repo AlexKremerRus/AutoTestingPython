e6d5cb8335df7adcf7230e0f4d1a9ee08588c7cd
a.nekhoroshev <Alexey.Nekhoroshev@advalange.com>
2022-12-01 15:00:39 +0300

    new file elements_test

--- Tests/elements_test.py
+++ Tests/elements_test.py
@@ -1,10 +1,15 @@
 import time
 
 from pages.base_page import BasePage
+from pages.elements_page import TextBoxPage
 
 
-def test(driver):
-    page = BasePage(driver, "https://www.google.com/")
-    page.open()
-    time.sleep(3)
-
+class TestElements:
+    class TestTextBox:
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            text_box_page.fill_all_fields()
+            output_name,output_email, output_cur_addr, output_per_addr=text_box_page.check_filled_form()
+            print(output_name,output_email, output_cur_addr, output_per_addr)
+            #time.sleep(5)
