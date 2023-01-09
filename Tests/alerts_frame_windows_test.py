e07fd8efc3f2c301172ef363d0fb33186efd36ae
a.nekhoroshev <Alexey.Nekhoroshev@advalange.com>
2023-01-09 19:20:05 +0300

    update files end 14 video

--- Tests/alerts_frame_windows_test.py
+++ Tests/alerts_frame_windows_test.py
@@ -1,5 +1,9 @@
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 import time
+
+
+class AlertPage:
+    pass
 
 
 class TesteAlertFrameWimdow:
@@ -17,3 +21,28 @@ class TesteAlertFrameWimdow:
             text_result=new_window_page.check_opened_new_tab()
             assert text_result=="This is a sample page", 'значения не совпадают'
 
+    class TestAlertsPage:
+
+        def test_see_alert(self,driver):
+            alert_page=AlertsPage(driver,"https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text=alert_page.check_see_alert()
+            assert alert_text=='You clicked a button'
+
+        def test_after_5_sec_alert(self,driver):
+            alert_page=AlertsPage(driver,"https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text=alert_page.check_alert_appear_5_sec()
+            assert alert_text=='This alert appeared after 5 seconds'
+
+        def test_confirm_alert(self,driver):
+            alert_page=AlertsPage(driver,"https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text=alert_page.check_confirm_alert()
+            assert alert_text=='You selected Ok'
+
+        def test_prompt_alert(self,driver):
+            alert_page=AlertsPage(driver,"https://demoqa.com/alerts")
+            alert_page.open()
+            text, alert_text=alert_page.check_prompt_alert()
+            assert alert_text==f'You entered {text}'
--- locators/alerts_frame_windows_page_locators.py
+++ locators/alerts_frame_windows_page_locators.py
@@ -5,3 +5,12 @@ class BrowserWindowsPageLocators:
     NEW_TAB_BUTTON=(By.CSS_SELECTOR, "button[id='tabButton']")
     TITLE_NEW_TAB=(By.CSS_SELECTOR,"h1[id='sampleHeading']")
     NEW_WINDOW_BUTTON=(By.CSS_SELECTOR, "button[id='windowButton']")
+
+
+class AlertsPageLocators:
+    SEE_ALERT_BUTTON=(By.CSS_SELECTOR, "button[id='alertButton']")
+    APPEAR_ALERT_AFTER_5_SEC_BUTTON=(By.CSS_SELECTOR, "button[id='timerAlertButton']")
+    CONFIRM_BOX_ALERT_BUTTON=(By.CSS_SELECTOR, "button[id='confirmButton']")
+    PROMPT_BOX_ALERT_BUTTON=(By.CSS_SELECTOR, "button[id='promtButton']")
+    RESULT_TEXT_CONFIRM=(By.CSS_SELECTOR, "span[id='confirmResult']")
+    RESULT_TEXT_PROMPT=(By.CSS_SELECTOR,"span[id='promptResult']")
