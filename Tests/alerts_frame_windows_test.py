117ee3208d3d990dee5ebcdc050c26db9a579d57
a.nekhoroshev <Alexey.Nekhoroshev@advalange.com>
2023-02-07 14:19:38 +0300

    update files add test for Frames end 15 video

--- Tests/alerts_frame_windows_test.py
+++ Tests/alerts_frame_windows_test.py
@@ -1,4 +1,4 @@
-from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage
 import time
 
 
@@ -46,3 +46,13 @@ class TesteAlertFrameWimdow:
             alert_page.open()
             text, alert_text=alert_page.check_prompt_alert()
             assert alert_text==f'You entered {text}'
+
+    class TestFramesPage:
+
+        def test_frames(self, driver):
+            frame_page=FramesPage(driver, "https://demoqa.com/frames")
+            frame_page.open()
+            result_frame_1=frame_page.check_frame('frame1')
+            result_frame_2=frame_page.check_frame('frame2')
+            assert result_frame_1 == ['This is a sample page', '500px', '350px'] , 'The Frame does not exist'
+            assert result_frame_2 == ['This is a sample page', '100px', '100px'], 'The Frame does not exist'
--- locators/alerts_frame_windows_page_locators.py
+++ locators/alerts_frame_windows_page_locators.py
@@ -14,3 +14,8 @@ class AlertsPageLocators:
     PROMPT_BOX_ALERT_BUTTON=(By.CSS_SELECTOR, "button[id='promtButton']")
     RESULT_TEXT_CONFIRM=(By.CSS_SELECTOR, "span[id='confirmResult']")
     RESULT_TEXT_PROMPT=(By.CSS_SELECTOR,"span[id='promptResult']")
+
+class FramesPageLocators:
+    FIRST_FRAME=(By.CSS_SELECTOR,"iframe[id='frame1']")
+    SECOND_FRAME=(By.CSS_SELECTOR,"iframe[id='frame2']")
+    TITLE_FRAME=(By.CSS_SELECTOR,"h1[id='sampleHeading']")
